63b688f5c5b52a9644839a75de048894991f37e4
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-15 19:11:09 -0400

    commit tree b604fe50404b4d4b9f9b6783790ada3f2e4138b0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(createFlair="*") {
+    return function(word="special") {
+        return `You are ${createFlair}${word}${createFlair}!`;
+    }
+}
